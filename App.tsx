d6deab592abdb8319bc88fc40a37c3fe359ec0b0
piotr.blazejak@yahoo.com <piotr.blazejak@yahoo.com>
2022-10-10 12:17:23 +0200

    fixed nav styling

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NewsFeedPostScreen, NewsFeedScreen} from './app/screens';
 import {NewsFeedDetailsScreen} from './app/screens/news-feed-details-screen/news-feed-details-screen';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {View} from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -11,6 +13,7 @@ const Stack = createNativeStackNavigator();
 const Home = () => {
   return (
     <Stack.Navigator
+      initialRouteName="NewsFeed"
       screenOptions={{
         headerShown: false,
       }}>
@@ -34,10 +37,41 @@ const App = () => {
       <Tab.Navigator
         screenOptions={{
           headerShown: false,
+          tabBarStyle: {
+            backgroundColor: '#4FCBC2',
+          },
         }}>
-        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarActiveTintColor: '#fff',
+            tabBarInactiveTintColor: '#fff',
+            tabBarIcon: ({focused}) => {
+              return (
+                <View>
+                  <Icon name="home" size={26} color="#fff" />
+                </View>
+              );
+            },
+          }}
+        />
 
-        <Tab.Screen name="Postfeed" component={NewsFeedPostScreen} />
+        <Tab.Screen
+          name="Postfeed"
+          component={NewsFeedPostScreen}
+          options={{
+            tabBarActiveTintColor: '#fff',
+            tabBarInactiveTintColor: '#fff',
+            tabBarIcon: ({focused}) => {
+              return (
+                <View>
+                  <Icon name="message1" size={26} color="#fff" />
+                </View>
+              );
+            },
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
